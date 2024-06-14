d38ce0ebdc096178431cc57269d9971ffa710a13
cenwadike <chinedunwadike04@gmail.com>
2024-06-14 06:46:14 +0100

    add http endpoints

---
+++ api/server.go
@@ -0,0 +1,27 @@
+package api
+
+import (
+	db "github.com/cenwadike/simplebank/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	router.GET("/accounts/", server.listAccounts)
+
+	server.router = router
+	return server
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
